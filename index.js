2056d2c9aa5902e70884a209403343397bfe1371
Tyler <schultty@mail.uc.edu>
2023-04-03 15:55:55 -0400

    time measurement changes

--- index.js
+++ index.js
@@ -10,25 +10,32 @@ const { Client } = require('@notionhq/client');
 const notion = new Client({ auth: process.env.AUTH });
 const bodyParser = require('body-parser');
 const cron = require('node-cron');
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-cron.schedule('0 30 20 * * *', () => {
-    client.messages.create({
-        body: 'Daily Journal: What are some things you did well today? What are some things you could have done better? What are somethings you learned/can improve upon?',
-        to: process.env.USERNUMBER, // Replace with the recipient's phone number
-        from: process.env.TWILIONUMBER // Replace with your Twilio phone number
-    })
-        .then(message => {
-            res.send('SMS sent!');
+setInterval(() => {
+    msgTime= "15 58 0"
+    const currentTime = new Date();
+    const time = `${currentTime.getHours()} ${currentTime.getMinutes()} ${currentTime.getSeconds()}`
+    console.log(time);
+    if(time===msgTime){
+        client.messages.create({
+            body: 'Daily Journal: What are some things you did well today? What are some things you could have done better? What are somethings you learned/can improve upon?',
+            to: process.env.USERNUMBER, // Replace with the recipient's phone number
+            from: process.env.TWILIONUMBER // Replace with your Twilio phone number
         })
-        .catch(error => {
-            console.log(error);
-            res.send('Error sending SMS');
-        }
-        )
-});
+            .then(message => {
+               console.log(message)
+            })
+            .catch(error => {
+                console.log(error);
+                
+            }
+            )
+    }
+}, 1000);
 
 app.post('/', (req, res) => {
     const { From, Body } = req.body;
